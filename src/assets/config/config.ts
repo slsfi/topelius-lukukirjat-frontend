665d8d13112b22998a636ff587c9b7e5ed343bb1
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2026-03-20 13:34:27 +0200

    refactor: enable feature based routes and separate cookie policy page in config

--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -18,8 +18,7 @@ export const config: Config = {
     },
     enableRouterLoadingBar: true,
     auth: {
-      enabled: false,
-      backendAuthBaseURL: "https://api.sls.fi/"
+      enabled: false
     },
     openGraphMetaTags: {
       enabled: true,
@@ -37,7 +36,7 @@ export const config: Config = {
     prebuild: {
       sitemap: false,
       staticCollectionMenus: false,
-      featureBasedRoutes: false
+      featureBasedRoutes: true
     },
     ssr: {
       trustProxyHops: 2,
@@ -271,7 +270,7 @@ export const config: Config = {
         indexPlaces: false,
         indexWorks: false,
         search: true,
-        cookiePolicy: false,
+        cookiePolicy: true,
         termsOfUse: false,
         privacyPolicy: false,
         accessibilityStatement: false,
